fd8e80f1655db58742e9366c93a88312fa727661
51L3N7 <53512454+1RF4@users.noreply.github.com>
2020-01-10 19:52:02 +0530

    Update mallu-dark-attackers.py

--- mallu-dark-attackers.py
+++ mallu-dark-attackers.py
@@ -31,7 +31,7 @@ def findAdmin():
 			print "Admin Panel Founded => ",req_link
  
 def Credit():
-	Space(9); print"(G +   Admin Panel Finder "
+	Space(9); print(G +"   Admin Panel Finder "
 	Space(9); print"        Script by 1RF4N "
 	Space(9); print"      MALLU DARK ATTACKERS)"
 Credit()
